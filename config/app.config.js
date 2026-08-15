31f060557aa9fa06301d0499459fba8026bb88d4
captthirumalai <CAPT.VTK@GMAIL.COM>
2026-08-15 18:14:33 +0530

    feat: compress entire UI to a 70% scale via root zoom so more information fits per screen; bump version to V0.3.6

--- config/app.config.js
+++ config/app.config.js
@@ -1,6 +1,6 @@
 export const appConfig = {
   appName: 'VAMS V2',
-  releaseVersion: 'V0.3.5',
+  releaseVersion: 'V0.3.6',
   defaultRoute: '/dashboard',
   theme: 'light'
 };
--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const config = {
   appName: 'VAMS V2',
-  version: 'V0.3.5'
+  version: 'V0.3.6'
 };
